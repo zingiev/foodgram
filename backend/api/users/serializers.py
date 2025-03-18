9fa3c5dd43ae816f5731ea4bc2fff1a9c94c332e
Yasin <yasinzingiev@mail.ru>
2025-03-18 11:25:47 +0300

    subscribe

--- backend/api/users/serializers.py
+++ backend/api/users/serializers.py
@@ -7,10 +7,17 @@ from rest_framework import serializers
 
 from api.validators import username_by_path_me, username_by_pattern
 from users.models import Subscription
-from core.decodeimage import Base64ImageField
+from recipes.models import Recipes
 
 
 User = get_user_model()
+
+
+def get_is_subscribed(request, obj):
+    if not request or not request.user.is_authenticated:
+        return False
+    return Subscription.objects.filter(
+        author=request.user, user=obj).exists()
 
 
 class CustomUserCreateSerializer(UserCreateSerializer):
@@ -36,7 +43,42 @@ class CustomUserSerialier(UserSerializer):
 
     def get_is_subscribed(self, obj):
         request = self.context.get('request')
-        if not request or not request.user.is_authenticated:
-            return False
-        return Subscription.objects.filter(
-            author=request.user, user=obj).exists()
+        return get_is_subscribed(request, obj)
+
+
+class RecipeMinifiedSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Recipes
+        fields = ('id', 'name', 'image', 'cooking_time')
+
+
+class UserSubscribeSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(source='author.email', read_only=True)
+    id = serializers.IntegerField(source='author.id', read_only=True)
+    username = serializers.CharField(source='author.username', read_only=True)
+    first_name = serializers.CharField(
+        source='author.first_name', read_only=True)
+    last_name = serializers.CharField(
+        source='author.last_name', read_only=True)
+    is_subscribed = serializers.SerializerMethodField()
+    recipes = serializers.SerializerMethodField()
+    recipes_count = serializers.SerializerMethodField()
+    avatar = serializers.ImageField(source='author.avatar', read_only=True)
+
+    class Meta:
+        model = Subscription
+        fields = ('email', 'id', 'username', 'first_name', 'last_name',
+                  'is_subscribed', 'recipes', 'recipes_count', 'avatar')
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+        return get_is_subscribed(request, obj)
+
+    def get_recipes(self, obj):
+        recipes = Recipes.objects.filter(author=obj.author)
+        return RecipeMinifiedSerializer(
+            recipes, many=True, context=self.context).data
+
+    def get_recipes_count(self, obj):
+        return Recipes.objects.filter(author=obj.author).count()
--- backend/api/users/views.py
+++ backend/api/users/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-from .serializers import CustomUserSerialier
+from users.models import Subscription
+from .serializers import CustomUserSerialier, UserSubscribeSerializer
 
 
 User = get_user_model()
@@ -18,3 +22,36 @@ class CustomUserViewSet(UserViewSet):
 
     def get_queryset(self):
         return self.queryset
+
+
+class UserSubscribeViewSet(viewsets.ModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = UserSubscribeSerializer
+    permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'post', 'delete']
+    pagination_class = PageNumberPagination
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    @staticmethod
+    def get_subscribe(request, kwargs):
+        author_id = kwargs.get('author_id')
+        author = get_object_or_404(User, pk=author_id)
+        subscribe = Subscription.objects.filter(
+            author=author, user=request.user)
+        return (author, subscribe)
+
+    def create(self, request, **kwargs):
+        author, subscribe = self.get_subscribe(request, kwargs)
+        if not subscribe.exists() and author != request.user:
+            subscribe.create(author=author, user=request.user)
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, **kwargs):
+        author, subscribe = self.get_subscribe(request, kwargs)
+        if subscribe.exists():
+            subscribe.delete()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
