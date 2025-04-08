4de5b1b20f2a9a0ac10198c81ba5113d2df0a324
zingiev <m@zingiev.ru>
2025-04-08 23:13:44 +0300

    isort

--- backend/api/views.py
+++ backend/api/views.py
@@ -15,9 +15,9 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from .filters import IngredientsSearchFilter, RecipeFilter
-from .permissions import IsAuthorOrReadOnly
 from .mixins import ShoppingFavoriteViewSet
 from .pagination import IngredientPagination, TagPagination
+from .permissions import IsAuthorOrReadOnly
 from .serializers import (FavoriteSerializer, IngredientSerializer,
                           RecipeSerializer, ShoppingCartSerializer,
                           TagSerializer)
